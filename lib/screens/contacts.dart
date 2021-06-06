a9d7f16ee182c8c3844d1fd771fb9b6c1b33776a
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-06 23:57:34 +0530

    Added contacts screen

---
+++ lib/screens/contacts.dart
@@ -0,0 +1,195 @@
+import 'package:chatybee/widgets/contacts_search.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../widgets/Snackbar.dart';
+
+class ContactsList extends StatefulWidget {
+  ContactsList({Key key}) : super(key: key);
+
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // Iterable<Contact> contacts = await ContactsService.getContacts(withThumbnails: false);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Theme.of(context).hintColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            splashRadius: 24,
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context)),
+        titleSpacing: 5,
+        title: FittedBox(
+            fit: BoxFit.scaleDown,
+            child:
+                Text('Contacts', style: Theme.of(context).textTheme.headline3)),
+        actions: <Widget>[
+          IconButton(
+            splashRadius: 24,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () {
+              showSearch(
+                  context: context,
+                  delegate: ContactsSearch(this.result ?? []));
+            },
+          ),
+          // IconButton(
+          //   splashRadius: 24,
+          //   icon: Icon(Icons.refresh),
+          //   tooltip: 'Refresh',
+          //   onPressed: () {
+          //     // handle the press
+          //   },
+          // ),
+        ],
+      ),
+      body: Snackbar(
+        child: Container(
+          width: size.width,
+          height: double.infinity,
+          child: FutureBuilder(
+              future: getContacts(),
+              builder: (cntx, AsyncSnapshot<Iterable<Contact>> snapshot) {
+                if (snapshot.data == null) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<Contact> contacts = snapshot.data.toList() ?? [];
+                List<String> phones = [];
+                for (Contact contact in contacts) {
+                  phones.addAll(contact.phones.map((phone) => phone.value));
+                }
+                return StreamBuilder(
+                    stream: firestore.collection('users').snapshots(),
+                    builder: (ctx,
+                        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                            snap) {
+                      if (snap.data == null) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      List<QueryDocumentSnapshot<Map<String, dynamic>>> users =
+                          snap.data.docs ?? [];
+                      return buildList(phones, users);
+                    });
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget buildList(List<String> phones,
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> users) {
+    List<QueryDocumentSnapshot<Map<String, dynamic>>> result =
+        shuffleBothLists(phones, users);
+    return ListView.builder(
+      physics: ClampingScrollPhysics(),
+      itemCount: result.length,
+      itemBuilder: (ctx, index) {
+        var contact = result[index];
+        final Widget avatar = contact["avatar"] != null
+            ? Image.network(contact["avatar"])
+            : Image.asset('assets/images/user.png');
+        return Material(
+          type: MaterialType.card,
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed('/chat', arguments: contact.id);
+            },
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ListTile(
+                  dense: true,
+                  horizontalTitleGap: 10,
+                  minVerticalPadding: 8,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                  isThreeLine: true,
+                  leading: CircleAvatar(
+                      radius: 26,
+                      child: Stack(children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              border: Border.all(color: Colors.grey)),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: avatar),
+                        ),
+                        if (contact["online"])
+                          Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Container(
+                                width: 15,
+                                height: 15,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color: Colors.green,
+                                    border: Border.all(
+                                        color: Theme.of(context).hintColor)),
+                              ))
+                      ])),
+                  title: Text(contact["name"] ?? "",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: Theme.of(context).textTheme.headline5.copyWith(
+                          fontSize: 18.0, fontWeight: FontWeight.w600)),
+                  subtitle: Text(contact["status"] ?? "",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(fontWeight: FontWeight.w200)),
+                )),
+          ),
+        );
+      },
+    );
+  }
+
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> shuffleBothLists(
+      List<String> phones,
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> users) {
+    List<QueryDocumentSnapshot<Map<String, dynamic>>> result = [];
+
+    for (QueryDocumentSnapshot<Map<String, dynamic>> s in users) {
+      if (phones.contains(s["phone"]) && !result.contains(s)) {
+        result.add(s);
+      }
+    }
+
+    result.sort((a, b) => a["name"].compareTo(b["name"]));
+
+    this.result = result;
+
+    return result;
+  }
+
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> result;
+
+  Future<Iterable<Contact>> getContacts() async {
+    PermissionStatus status = await Permission.contacts.request();
+    if (status == PermissionStatus.granted ||
+        status == PermissionStatus.limited) {
+      return ContactsService.getContacts(withThumbnails: false);
+    }
+    return null;
+  }
+}
