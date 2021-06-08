763ceaee83b134c319c0f3ebe95bba689fb61b78
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-08 20:20:07 +0530

    Fixed left drag animation issue

--- lib/widgets/swipeable.dart
+++ lib/widgets/swipeable.dart
@@ -62,54 +62,56 @@ class _SwipeableState extends State<Swipeable> with TickerProviderStateMixin {
     var oldDragExtent = _dragExtent;
     _dragExtent += delta;
 
-    if (oldDragExtent.sign != _dragExtent.sign) {
-      setState(() {
-        _updateMoveAnimation();
-      });
+    if (_dragExtent > 0) {
+      if (oldDragExtent.sign != _dragExtent.sign) {
+        setState(() {
+          _updateMoveAnimation();
+        });
+      }
+
+      var movePastThresholdPixels = widget.threshold;
+      var newPos = _dragExtent.abs() / context.size.width;
+
+      if (_dragExtent.abs() > movePastThresholdPixels) {
+        // how many "thresholds" past the threshold we are. 1 = the threshold 2
+        // = two thresholds.
+        var n = _dragExtent.abs() / movePastThresholdPixels;
+
+        // Take the number of thresholds past the threshold, and reduce this
+        // number
+        var reducedThreshold = math.pow(n, 0.3);
+
+        var adjustedPixelPos = movePastThresholdPixels * reducedThreshold;
+        newPos = adjustedPixelPos / context.size.width;
+
+        if (_dragExtent > 0 && !_pastLeftThreshold) {
+          _pastLeftThreshold = true;
+
+          if (widget.onSwipeRight != null) {
+            widget.onSwipeRight();
+          }
+        }
+        if (_dragExtent < 0 && !_pastRightThreshold) {
+          _pastRightThreshold = true;
+
+          if (widget.onSwipeLeft != null) {
+            widget.onSwipeLeft();
+          }
+        }
+      } else {
+        // Send a cancel event if the user has swiped back underneath the
+        // threshold
+        if (_pastLeftThreshold || _pastRightThreshold) {
+          if (widget.onSwipeCancel != null) {
+            widget.onSwipeCancel();
+          }
+        }
+        _pastLeftThreshold = false;
+        _pastRightThreshold = false;
+      }
+
+      _moveController.value = newPos;
     }
-
-    var movePastThresholdPixels = widget.threshold;
-    var newPos = _dragExtent.abs() / context.size.width;
-
-    if (_dragExtent.abs() > movePastThresholdPixels) {
-      // how many "thresholds" past the threshold we are. 1 = the threshold 2
-      // = two thresholds.
-      var n = _dragExtent.abs() / movePastThresholdPixels;
-
-      // Take the number of thresholds past the threshold, and reduce this
-      // number
-      var reducedThreshold = math.pow(n, 0.3);
-
-      var adjustedPixelPos = movePastThresholdPixels * reducedThreshold;
-      newPos = adjustedPixelPos / context.size.width;
-
-      if (_dragExtent > 0 && !_pastLeftThreshold) {
-        _pastLeftThreshold = true;
-
-        if (widget.onSwipeRight != null) {
-          widget.onSwipeRight();
-        }
-      }
-      if (_dragExtent < 0 && !_pastRightThreshold) {
-        _pastRightThreshold = true;
-
-        if (widget.onSwipeLeft != null) {
-          widget.onSwipeLeft();
-        }
-      }
-    } else {
-      // Send a cancel event if the user has swiped back underneath the
-      // threshold
-      if (_pastLeftThreshold || _pastRightThreshold) {
-        if (widget.onSwipeCancel != null) {
-          widget.onSwipeCancel();
-        }
-      }
-      _pastLeftThreshold = false;
-      _pastRightThreshold = false;
-    }
-
-    _moveController.value = newPos;
   }
 
   void _handleDragEnd(DragEndDetails details) {
