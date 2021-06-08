caf661c2313b520af5defb51b299c998498bfb77
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-08 20:14:47 +0530

    Improved page route transtions

--- lib/animations/slideRoute.dart
+++ lib/animations/slideRoute.dart
@@ -16,13 +16,8 @@ class SlideTopRoute extends PageRouteBuilder {
             Animation<double> secondaryAnimation,
             Widget child,
           ) =>
-              SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(0, 1),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          ),
+              FadeUpwardsPageTransition(
+                  routeAnimation: animation, child: child),
         );
 }
 
@@ -42,38 +37,82 @@ class SlideRightRoute extends PageRouteBuilder {
             Animation<double> secondaryAnimation,
             Widget child,
           ) =>
-              SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          ),
+              FadeRightPageTransition(routeAnimation: animation, child: child),
         );
 }
 
-class SlideLeftRoute extends PageRouteBuilder {
-  final Widget page;
-  SlideLeftRoute({this.page})
-      : super(
-          pageBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-          ) =>
-              page,
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) =>
-              SlideTransition(
-            position: Tween<Offset>(
-              begin: const Offset(-1, 0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child,
-          ),
-        );
+class FadeUpwardsPageTransition extends StatelessWidget {
+  FadeUpwardsPageTransition({
+    Key key,
+    @required
+        Animation<double>
+            routeAnimation, // The route's linear 0.0 - 1.0 animation.
+    @required this.child,
+  })  : _positionAnimation =
+            routeAnimation.drive(_bottomUpTween.chain(_fastOutSlowInTween)),
+        _opacityAnimation = routeAnimation.drive(_easeInTween),
+        super(key: key);
+
+  // Fractional offset from 1/4 screen below the top to fully on screen.
+  static final Tween<Offset> _bottomUpTween = Tween<Offset>(
+    begin: const Offset(0.0, 0.25),
+    end: Offset.zero,
+  );
+  static final Animatable<double> _fastOutSlowInTween =
+      CurveTween(curve: Curves.fastOutSlowIn);
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.easeIn);
+
+  final Animation<Offset> _positionAnimation;
+  final Animation<double> _opacityAnimation;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _positionAnimation,
+      child: FadeTransition(
+        opacity: _opacityAnimation,
+        child: child,
+      ),
+    );
+  }
+}
+
+class FadeRightPageTransition extends StatelessWidget {
+  FadeRightPageTransition({
+    Key key,
+    @required
+        Animation<double>
+            routeAnimation, // The route's linear 0.0 - 1.0 animation.
+    @required this.child,
+  })  : _positionAnimation =
+            routeAnimation.drive(_bottomUpTween.chain(_fastOutSlowInTween)),
+        _opacityAnimation = routeAnimation.drive(_easeInTween),
+        super(key: key);
+
+  // Fractional offset from 1/4 screen below the top to fully on screen.
+  static final Tween<Offset> _bottomUpTween = Tween<Offset>(
+    begin: const Offset(0.25, 0),
+    end: Offset.zero,
+  );
+  static final Animatable<double> _fastOutSlowInTween =
+      CurveTween(curve: Curves.fastOutSlowIn);
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.easeIn);
+
+  final Animation<Offset> _positionAnimation;
+  final Animation<double> _opacityAnimation;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _positionAnimation,
+      child: FadeTransition(
+        opacity: _opacityAnimation,
+        child: child,
+      ),
+    );
+  }
 }
