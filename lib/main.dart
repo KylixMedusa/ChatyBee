0bf6604cc69ffd26bfccf25fca177630143a35d3
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-08 20:14:21 +0530

    Added routes and theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,17 @@ import 'screens/contacts.dart';
 import 'screens/home.dart';
 import 'screens/login.dart';
 import 'screens/register.dart';
+import 'screens/settings.dart';
 import 'screens/splash_screen.dart';
 import 'screens/welcome.dart';
 import 'screens/phone_verify.dart';
 import 'theme/theme.dart';
 import 'animations/slideRoute.dart';
 import 'screens/profile_add.dart';
+import 'theme/themeModel.dart';
 
 final SnackbarStore snackbarStore = SnackbarStore();
+final FlutterSecureStorage storage = FlutterSecureStorage();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -44,50 +47,60 @@ class _MyAppState extends State<MyApp> {
       child: MultiProvider(
         providers: [
           Provider<SnackbarStore>(
-            create: (_) => SnackbarStore(),
+            create: (_) => snackbarStore,
           ),
           Provider<FlutterSecureStorage>(
-            create: (_) => FlutterSecureStorage(),
-          )
+            create: (_) => storage,
+          ),
         ],
-        child: MaterialApp(
-          theme: kLightTheme,
-          debugShowCheckedModeBanner: false,
-          home: CustomSplashScreen(),
-          onGenerateRoute: (settings) {
-            switch (settings.name) {
-              case '/login':
-                return SlideRightRoute(page: Login());
-                break;
-              case '/register':
-                return SlideRightRoute(page: Register());
-                break;
-              case '/profile-add':
-                return SlideRightRoute(page: ProfileAdd());
-                break;
-              case '/phone-verify':
-                return SlideRightRoute(page: PhoneVerify());
-                break;
-              case '/contacts':
-                return SlideTopRoute(page: ContactsList());
-                break;
-              case '/home':
-                return FadeRoute(page: Home());
-                break;
-              case '/welcome':
-                return FadeRoute(page: Welcome());
-                break;
-              case '/chat':
-                final String id = settings.arguments;
-                return SlideRightRoute(
-                    page: ChatPage(
-                  recieverId: id,
-                ));
-                break;
-              default:
-                return MaterialPageRoute(builder: (ctx) => Welcome());
-            }
-          },
+        child: ChangeNotifierProvider<ThemeModel>(
+          create: (_) => ThemeModel(storage),
+          child: Consumer<ThemeModel>(builder: (_, model, __) {
+            return MaterialApp(
+              theme: kLightTheme,
+              darkTheme: kDarkTheme,
+              debugShowCheckedModeBanner: false,
+              themeMode: model.mode,
+              home: CustomSplashScreen(),
+              onGenerateRoute: (settings) {
+                switch (settings.name) {
+                  case '/login':
+                    return SlideRightRoute(page: Login());
+                    break;
+                  case '/register':
+                    return SlideRightRoute(page: Register());
+                    break;
+                  case '/profile-add':
+                    return SlideRightRoute(page: ProfileAdd());
+                    break;
+                  case '/phone-verify':
+                    return SlideRightRoute(page: PhoneVerify());
+                    break;
+                  case '/contacts':
+                    return SlideTopRoute(page: ContactsList());
+                    break;
+                  case '/home':
+                    return FadeRoute(page: Home());
+                    break;
+                  case '/settings':
+                    return FadeRoute(page: Settings());
+                    break;
+                  case '/welcome':
+                    return FadeRoute(page: Welcome());
+                    break;
+                  case '/chat':
+                    final String id = settings.arguments;
+                    return SlideRightRoute(
+                        page: ChatPage(
+                      recieverId: id,
+                    ));
+                    break;
+                  default:
+                    return MaterialPageRoute(builder: (ctx) => Welcome());
+                }
+              },
+            );
+          }),
         ),
       ),
     );
