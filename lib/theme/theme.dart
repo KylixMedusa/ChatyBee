23f2878231a073608aa226611e2577a09d79f60e
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-08 20:18:18 +0530

    Added dark theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import './config.dart' as config;
+import './config.dart';
 
 var kLightTheme = ThemeData(
   canvasColor: Colors.transparent,
   primaryColor: Colors.white,
   brightness: Brightness.light,
-  accentColor: config.Colors().accentColor(1),
-  focusColor: config.Colors().mainColor(1),
-  hintColor: config.Colors().secondColor(1),
+  accentColor: CustomColors().accentColor(1),
+  focusColor: CustomColors().mainColor(1),
+  hintColor: CustomColors().secondColor(1),
   accentTextTheme:
       TextTheme(headline6: TextStyle(fontFamily: "Red Hat Display")),
   textTheme: TextTheme(
@@ -26,7 +26,7 @@ var kLightTheme = ThemeData(
         fontSize: 16,
         fontFamily: "Red Hat Display",
         fontWeight: FontWeight.w500,
-        color: config.Colors().accentColor(1)),
+        color: CustomColors().accentColor(1)),
     headline3: TextStyle(
         fontSize: 20,
         fontFamily: "Red Hat Display",
@@ -39,14 +39,14 @@ var kLightTheme = ThemeData(
         color: Colors.black),
     headline1: TextStyle(
       fontFamily: 'Red Hat Display',
-      color: config.Colors().accentColor(1),
+      color: CustomColors().accentColor(1),
       fontSize: 50,
       fontWeight: FontWeight.w600,
     ),
     subtitle1: TextStyle(
       fontSize: 20,
       fontWeight: FontWeight.w900,
-      color: config.Colors().secondColor(1),
+      color: CustomColors().secondColor(1),
       fontFamily: "Roboto",
     ),
     headline6: TextStyle(
@@ -70,80 +70,81 @@ var kLightTheme = ThemeData(
       fontFamily: 'Roboto',
       fontSize: 16,
       fontWeight: FontWeight.w400,
-      color: config.Colors().accentColor(1),
+      color: CustomColors().accentColor(1),
     ),
   ),
 );
 
-// var kDarkTheme = ThemeData(
-//   canvasColor: Colors.transparent,
-//   primaryColor: Color(0xFF181818),
-//   brightness: Brightness.dark,
-//   accentColor: config.Colors().accentDarkColor(1),
-//   focusColor: config.Colors().mainDarkColor(1),
-//   hintColor: config.Colors().secondDarkColor(1),
-//   accentTextTheme: TextTheme(headline6: TextStyle(fontFamily: "Red Hat Display")),
-//   textTheme: TextTheme(
-//     button: TextStyle(
-//       fontFamily: 'Red Hat Display',
-//       fontSize: 16,
-//       fontWeight: FontWeight.w800,
-//       color: Color(0xFF181818),
-//     ),
-//     headline5: TextStyle(
-//       fontSize: 16.0,
-//       color: config.Colors().accentDarkColor(1),
-//       fontFamily: "Red Hat Display",
-//     ),
-//     headline4: TextStyle(
-//         fontSize: 16,
-//         fontFamily: "Red Hat Display",
-//         fontWeight: FontWeight.w500,
-//         color: config.Colors().accentDarkColor(1)),
-//     headline3: TextStyle(
-//         fontSize: 20,
-//         fontFamily: "Red Hat Display",
-//         fontWeight: FontWeight.w500,
-//         color: Colors.white),
-//     headline2: TextStyle(
-//         fontSize: 24,
-//         fontFamily: "Red Hat Display",
-//         fontWeight: FontWeight.w500,
-//         color: Colors.white),
-//     headline1: TextStyle(
-//       fontFamily: 'Red Hat Display',
-//       color: config.Colors().accentDarkColor(1),
-//       fontSize: 50,
-//       fontWeight: FontWeight.w600,
-//     ),
-//     subtitle1: TextStyle(
-//       fontSize: 20,
-//       fontWeight: FontWeight.w900,
-//       color: config.Colors().secondDarkColor(1),
-//       fontFamily: "Roboto",
-//     ),
-//     headline6: TextStyle(
-//       fontSize: 14.0,
-//       color: config.Colors().accentDarkColor(.85),
-//       fontFamily: "Red Hat Display",
-//     ),
-//     bodyText2: TextStyle(
-//       fontFamily: 'Red Hat Display',
-//       fontSize: 14,
-//       fontWeight: FontWeight.w500,
-//       color: config.Colors().accentDarkColor(.85),
-//     ),
-//     bodyText1: TextStyle(
-//       fontFamily: 'Red Hat Display',
-//       fontSize: 22,
-//       fontWeight: FontWeight.w500,
-//       color: config.Colors().accentDarkColor(1),
-//     ),
-//     caption: TextStyle(
-//       fontFamily: 'Roboto',
-//       fontSize: 16,
-//       fontWeight: FontWeight.w400,
-//       color: config.Colors().accentDarkColor(1),
-//     ),
-//   ),
-// );
+var kDarkTheme = ThemeData(
+  canvasColor: Colors.transparent,
+  primaryColor: Colors.black45,
+  brightness: Brightness.dark,
+  accentColor: CustomColors().accentDarkColor(1),
+  focusColor: CustomColors().mainDarkColor(1),
+  hintColor: CustomColors().secondDarkColor(1),
+  accentTextTheme:
+      TextTheme(headline6: TextStyle(fontFamily: "Red Hat Display")),
+  textTheme: TextTheme(
+    button: TextStyle(
+      fontFamily: 'Red Hat Display',
+      fontSize: 16,
+      fontWeight: FontWeight.w800,
+      color: Color(0xFF181818),
+    ),
+    headline5: TextStyle(
+      fontSize: 16.0,
+      color: Colors.grey[200],
+      fontFamily: "Red Hat Display",
+    ),
+    headline4: TextStyle(
+        fontSize: 16,
+        fontFamily: "Red Hat Display",
+        fontWeight: FontWeight.w500,
+        color: CustomColors().accentDarkColor(1)),
+    headline3: TextStyle(
+        fontSize: 20,
+        fontFamily: "Red Hat Display",
+        fontWeight: FontWeight.w500,
+        color: Colors.white),
+    headline2: TextStyle(
+        fontSize: 24,
+        fontFamily: "Red Hat Display",
+        fontWeight: FontWeight.w500,
+        color: Colors.white),
+    headline1: TextStyle(
+      fontFamily: 'Red Hat Display',
+      color: CustomColors().accentDarkColor(1),
+      fontSize: 50,
+      fontWeight: FontWeight.w600,
+    ),
+    subtitle1: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w900,
+      color: CustomColors().secondDarkColor(1),
+      fontFamily: "Roboto",
+    ),
+    headline6: TextStyle(
+      fontSize: 14.0,
+      color: CustomColors().accentDarkColor(.85),
+      fontFamily: "Red Hat Display",
+    ),
+    bodyText2: TextStyle(
+      fontFamily: 'Red Hat Display',
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: Colors.grey[350],
+    ),
+    bodyText1: TextStyle(
+      fontFamily: 'Red Hat Display',
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+      color: CustomColors().accentDarkColor(1),
+    ),
+    caption: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: CustomColors().accentDarkColor(1),
+    ),
+  ),
+);
