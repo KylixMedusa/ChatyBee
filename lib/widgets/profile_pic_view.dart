9ee19eb4ceaed4f6d84472fce46fed299b563289
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-06-08 20:19:49 +0530

    Added photohero animation

--- lib/widgets/profile_pic_view.dart
+++ lib/widgets/profile_pic_view.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../animations/PhotoHero.dart';
+
 class ProfilePicView extends StatelessWidget {
   final Widget picture;
   const ProfilePicView({Key key, this.picture}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(6.0),
-      child: Stack(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Theme.of(context).accentColor),
-                  borderRadius: BorderRadius.circular(100)),
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(100), child: picture)),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 15,
-                height: 15,
-                padding: EdgeInsets.all(2),
-                child:
-                    FittedBox(fit: BoxFit.scaleDown, child: Icon(Icons.menu)),
+    return PhotoHero(
+      page: 'home',
+      onTap: () {
+        Navigator.pushNamed(context, '/settings');
+      },
+      child: Container(
+        padding: EdgeInsets.all(6.0),
+        child: Stack(
+          children: [
+            Container(
                 decoration: BoxDecoration(
-                    border: Border.all(),
-                    color: Colors.white,
+                    border: Border.all(color: Theme.of(context).accentColor),
                     borderRadius: BorderRadius.circular(100)),
-              ))
-        ],
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100), child: picture)),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  padding: EdgeInsets.all(2),
+                  child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Icon(Icons.menu, color: Colors.black)),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100)),
+                ))
+          ],
+        ),
       ),
     );
   }
